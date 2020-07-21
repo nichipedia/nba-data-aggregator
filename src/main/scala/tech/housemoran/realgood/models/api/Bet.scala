4b9163ba62b2065d641a60b1d75f8ceb3c4ec4e4
Nicholas Moran <atreayou@gmail.com>
2020-07-20 19:50:42 -0500

    mer

--- src/main/scala/tech/housemoran/realgood/models/api/Bet.scala
+++ src/main/scala/tech/housemoran/realgood/models/api/Bet.scala
@@ -15,6 +15,7 @@ class Bet(val propType: PropType, val date: Date, val game: Game, val line: Line
     pick match {
       case Pick.underDog => money * (line.favorite / 100)
       case Pick.favorite => money / (line.underDog / 100)
+      case _ => 0
     }
   }
 
--- src/main/scala/tech/housemoran/realgood/scrapers/bets/NBABetScraper.scala
+++ src/main/scala/tech/housemoran/realgood/scrapers/bets/NBABetScraper.scala
@@ -1,16 +1,30 @@
 package tech.housemoran.realgood.scrapers.bets
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.jsoup.Jsoup
 import tech.housemoran.realgood.models.api.Bet
 import tech.housemoran.realgood.scrapers.api.BetScraper
+import java.util.Map
+
+import scala.collection.JavaConverters._
 
 class NBABetScraper extends BetScraper {
-  private val urlBase = "https://nj.foxbet.com/#/basketball/competitions"
+  private val urlBase = "https://www.oddsshark.com/nba/odds"
+  private val mapper = new ObjectMapper
 
   override def getCurrentBets: List[Bet] = {
     val resp = Jsoup.connect(urlBase).execute
-    val document = Jsoup.parse(resp.body)
-    println(document.body())
+    val doc = Jsoup.parse(resp.body)
+    doc.select("div[class=slidee] div[class=op-item-wrapper]")
+        .asScala
+        .foreach(line => {
+          val jsonDataString = line.child(0).child(0).attr("data-op-info")
+          val spreadData = mapper.readValue(jsonDataString, classOf[Map[String, String]]).asScala
+          println(spreadData.get("fullgame"))
+        })
+
+
+
     List.empty
   }
 }
